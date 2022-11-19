d6b9541ae7fa5c434d8f54c3a26dc0a9f5366ac9
shravyagrk@gmail.com <shravyagrk@gmail.com>
2022-11-19 21:13:02 +0100

    added navigation component to the container

--- packages/client/src/containers/LandingPage/LandingPage.Container.js
+++ packages/client/src/containers/LandingPage/LandingPage.Container.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './LandingPage.Style.css';
 import { ProductsList } from './ProductsList';
 import { products } from './Products';
+import NavigationComponent from '../../components/Navigation/Navigation.component';
 
 export const LandingPage = () => {
   return (
     <div className="landing-page-container">
+      <NavigationComponent />
       <div className="landing-page-header">
         <span>LANDING PAGE</span>
       </div>
