8ef7434db4c00d7a0f5cc8933c35b61d8d037804
shravyagrk@gmail.com <shravyagrk@gmail.com>
2022-11-19 21:12:06 +0100

    added route for products and swagger comments for the routes

--- packages/server/api/controllers/products.controller.js
+++ packages/server/api/controllers/products.controller.js
@@ -4,6 +4,17 @@ const getProducts = async () => {
   return knex('products').select('*');
 };
 
+const createProduct = async (body) => {
+  await knex('products').insert({
+    name: body.name,
+    description: body.description,
+  });
+  return {
+    successful: true,
+  };
+};
+
 module.exports = {
   getProducts,
+  createProduct,
 };
